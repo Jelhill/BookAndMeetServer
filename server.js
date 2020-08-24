67511b6937e7340aff625d4f6e3ada6e6ff148fa
Jelhill <talk2jelili2000@gmail.com>
2020-08-24 23:45:11 +0100

    cors

--- server.js
+++ server.js
@@ -9,10 +9,20 @@ const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
   });
+
 app.use(morgan("tiny"))
 app.use("/uploads", express.static("uploads"))
 
